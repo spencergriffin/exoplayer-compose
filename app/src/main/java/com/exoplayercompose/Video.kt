4f2d212a40e96e6f1be2d2427381555e7dcaebbe
Spencer Griffin <sgriffin@ancestry.com>
2021-06-11 17:01:53 -0600

    Initial commit

---
+++ app/src/main/java/com/exoplayercompose/Video.kt
@@ -0,0 +1,11 @@
+package com.exoplayercompose
+
+data class Video(
+    val description: String,
+    val url: String,
+    val subtitle: String,
+    val thumb: String,
+    val title: String,
+    val width: Int,
+    val height: Int
+)
